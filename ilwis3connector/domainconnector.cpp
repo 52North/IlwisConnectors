d10275e3f5c555ef56f86679c2fc377403d2d7e2
MSchouwenburg <m.schouwenburg@utwente.nl>
2013-08-07 15:30:21 +0200

    added type parameter for the store to parent connectors

--- ilwis3connector/domainconnector.cpp
+++ ilwis3connector/domainconnector.cpp
@@ -218,7 +218,7 @@ bool DomainConnector::storeMetaData(IlwisObject *data)
     QString alias = kernel()->database().findAlias(dmName,"domain","ilwis3");
     if ( alias != sUNDEF)
         return true; // nothing to be done, already exists as a system domain
-    Ilwis3Connector::storeMetaData(data);
+    Ilwis3Connector::storeMetaData(data, itDOMAIN);
 
     _odf->setKeyValue("Ilwis", "Type", "Domain");
     if ( dom->ilwisType() == itNUMERICDOMAIN) {
