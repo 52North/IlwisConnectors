aae62e305bc9cbc8ca08fa95a509968ea80b040e
pokulo@gmx.de <h.g.kiessling@utwente.nl>
2014-02-04 11:02:13 +0100

    added query for OperationMetaData to Python API

--- pythonapi/pythonapi_engine.h
+++ pythonapi/pythonapi_engine.h
@@ -11,6 +11,7 @@ namespace pythonapi {
         static Object* _do(std::string output_name, std::string operation,std::string c3 = "",std::string c4 = "",std::string c5 = "",std::string c6 = "",std::string c7="", std::string c8="", std::string c9="");
         static bool setWorkingCatalog(const std::string& location);
         PyObject* operations(const std::string& filter = "*");
+        std::string operationMetaData(const std::string& name);
     };
 
 }
