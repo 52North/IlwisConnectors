bf39d44d23868b6a97f3535fde38a1cab87590eb
Henning Bredel <h.bredel@52north.org>
2014-02-20 17:03:39 +0100

    add gdal connector's column callback strategy to parse attributes from stream to feature's attribute table

--- wfsconnector/wfsfeatureparser.cpp
+++ wfsconnector/wfsfeatureparser.cpp
@@ -30,14 +30,18 @@ WfsFeatureParser::WfsFeatureParser(WfsResponse *response)
     _parser->addNamespaceMapping("wfs", "http://www.opengis.net/wfs");
     _parser->addNamespaceMapping("ows", "http://www.opengis.net/ows");
     _parser->addNamespaceMapping("gml", "http://www.opengis.net/gml");
+
+    _columnFillers.resize(0);
 }
 
 WfsFeatureParser::~WfsFeatureParser()
 {
     delete _parser;
+    for(FillerColumnDef *def : _columnFillers)
+        delete def;
 }
 
-void WfsFeatureParser::parseFeature(ITable &table, QMap<QString, QString> &mappings) const
+void WfsFeatureParser::parseFeature(ITable &table, QMap<QString, QString> &mappings)
 {
     QString featureType = table->name();
     QString targetNamespace = mappings[""];
@@ -50,14 +54,19 @@ void WfsFeatureParser::parseFeature(ITable &table, QMap<QString, QString> &mappi
         if (_parser->moveToNext("gml:featureMembers")) {
             while (_parser->moveToNext(featureType)) {
                 quint64 cIdx = 0;
-                std::vector<QVariant> records(table->columnCount());
+                std::vector<QVariant> record(table->columnCount());
+
+                setColumnCallbacks(table);
+                loadRecord(table, record);
+
                 while (_parser->readNextStartElement()) {
                     QString attribute = _parser->name();
                     ColumnDefinition definition = table->columndefinition(attribute);
                     DataDefinition dataDef = definition.datadef();
                     IDomain domain = dataDef.domain();
+
                 }
-                table->record(featureCount++, records);
+                table->record(featureCount++, record);
             }
         }
         while (_parser->moveToNext("wfs:featureMember")) {
@@ -70,8 +79,94 @@ void WfsFeatureParser::parseFeature(ITable &table, QMap<QString, QString> &mappi
     table->column("ogi:quad100_id");
 }
 
-void WfsFeatureParser::parseFeatureAttribute(quint64 id, QString attribute) const
-{
-//    std::vector<QVariant> record(table->columnCount());
 
+void WfsFeatureParser::setColumnCallbacks(ITable &table)
+{
+    _columnFillers.resize(table->columnCount(),0);
+    for (int i = 0; i < table->columnCount(); i++){
+        ColumnDefinition& coldef = table->columndefinition(i);
+        if ( coldef.name() == QString(FEATUREIDCOLUMN) ) {
+            continue; // auto filled column
+        }
+
+        DataDefinition& datadef = coldef.datadef();
+        if( !datadef.domain().isValid()){
+            WARN2(ERR_NO_INITIALIZED_2, "domain", coldef.name());
+            continue;
+        }
+
+        IlwisTypes tp = datadef.domain()->valueType();
+        if (tp & itSTRING){
+            _columnFillers[i] = new FillerColumnDef(&WfsFeatureParser::fillStringColumn);
+        } else if (tp & itINTEGER){
+            NumericRange* r = static_cast<NumericRange*>(datadef.domain()->range2range<NumericRange>()->clone());
+            //creating the actual range as invalid to be adjusted in the fillers
+            double min = r->min();
+            r->min(r->max());
+            r->max(min);
+            datadef.range(r);
+            _columnFillers[i] = new FillerColumnDef(&WfsFeatureParser::fillIntegerColumn);
+        } else if (tp & itDOUBLE){
+            //creating the actual range as invalid to be adjusted in the fillers
+            NumericRange* r = static_cast<NumericRange*>(datadef.domain()->range2range<NumericRange>()->clone());
+            double min = r->min();
+            r->min(r->max());
+            r->max(min);
+            datadef.range(r);
+            _columnFillers[i] = new FillerColumnDef(&WfsFeatureParser::fillDoubleColumn);
+        } else if (tp & itTIME){
+            //creating the actual range as invalid to be adjusted in the fillers
+            NumericRange* r = static_cast<NumericRange*>(datadef.domain()->range2range<NumericRange>()->clone());
+            double min = r->min();
+            r->min(r->max());
+            r->max(min);
+            datadef.range(r);
+            _columnFillers[i] = new FillerColumnDef(&WfsFeatureParser::fillDateTimeColumn);
+        }
+    }
+}
+
+void WfsFeatureParser::loadRecord(ITable &table, std::vector<QVariant>& record )
+{
+    if (_columnFillers.size() > 0) {
+        for (int i = 0; i < table->columnCount();i++){
+            if (_columnFillers[i]){
+                record[i] = (this->*_columnFillers[i]->fillFunc)();
+            }else{
+                record[i] = QVariant();
+            }
+        }
+    }
+}
+
+QVariant WfsFeatureParser::fillStringColumn()
+{
+    QVariant v = QVariant(_parser->readElementText());
+    _parser->readNextStartElement();
+    return v;
+}
+
+QVariant WfsFeatureParser::fillIntegerColumn()
+{
+    QVariant v = QVariant(_parser->readElementText().toInt());
+    _parser->readNextStartElement();
+    return v;
+}
+
+QVariant WfsFeatureParser::fillDoubleColumn()
+{
+    QVariant v = QVariant(_parser->readElementText().toDouble());
+    _parser->readNextStartElement();
+    return v;
+}
+
+QVariant WfsFeatureParser::fillDateTimeColumn()
+{
+    Time time;
+
+    // TODO: parse xsd:time
+
+    QVariant v = QVariant(_parser->readElementText());
+    _parser->readNextStartElement();
+    return v;
 }
--- wfsconnector/wfsfeatureparser.h
+++ wfsconnector/wfsfeatureparser.h
@@ -10,6 +10,7 @@ namespace Ilwis {
 namespace Wfs {
 
 class WfsResponse;
+class FillerColumnDef;
 
 class WFSCONNECTORSHARED_EXPORT WfsFeatureParser
 {
@@ -17,12 +18,54 @@ public:
     WfsFeatureParser(WfsResponse *response);
     ~WfsFeatureParser();
 
-    void parseFeature(ITable &table, QMap<QString,QString> &mappings) const;
+    /**
+     * Parses feature instances to the given attribute table.<br/>
+     * <br/>
+     * Note that a WFS feature attribute table has to be constructed first to give
+     * valuable results. This can be done by means of a WfsFeatureDescriptionParser
+     * which uses the feature's schema description served by the WFS to create appropriate
+     * Domain columns for each feature attribute.<br/>
+     * <br/>
+     * The parsed xml schema description may use different namespace mappings than the
+     * feature collection. To keep track of the elements the WfsFeatureDescriptionParser
+     * gives access to those used in the schema document.
+     *
+     * TODO: for now only the target namespace is being used ... propably this is enough
+     *
+     * @param table the feature's attribute table.
+     * @param mappings the xml schema namespace mappings.
+     */
+    void parseFeature(ITable &table, QMap<QString,QString> &mappings);
+
+    /**
+     * Sets individual column handlers according to the domain associated with the given
+     * table.<br/>
+     * <br/>
+     * Note that a WFS feature attribute table has to be constructed first to give
+     * valuable results. This can be done by means of a WfsFeatureDescriptionParser
+     * which uses the feature's schema description served by the WFS to create appropriate
+     * Domain columns for each feature attribute.<br/>
+     *
+     * @param table the table having domain descriptions for each column.
+     */
+    void setColumnCallbacks(ITable &table);
 
 private:
     XmlStreamParser *_parser;
+    std::vector<FillerColumnDef*> _columnFillers;
 
-    void parseFeatureAttribute(quint64 id, QString attribute) const;
+    void loadRecord(ITable &table, std::vector<QVariant> &record);
+
+    QVariant fillStringColumn();
+    QVariant fillDoubleColumn();
+    QVariant fillDateTimeColumn();
+    QVariant fillIntegerColumn();
+
+};
+
+struct FillerColumnDef {
+    FillerColumnDef(QVariant(WfsFeatureParser::* func)()): fillFunc(func){}
+    QVariant (WfsFeatureParser::* fillFunc)();
 };
 
 }
