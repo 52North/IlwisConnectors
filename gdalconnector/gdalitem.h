40eee38ab66322b024b5cdbf24e93b44e1a4beb2
MSchouwenburg <m.schouwenburg@utwente.nl>
2014-03-17 09:41:40 +0100

    gdalitems also adds amd recognizes ogr formats that are containers and seperates them

--- gdalconnector/gdalitem.h
+++ gdalconnector/gdalitem.h
@@ -3,14 +3,14 @@
 
 namespace Ilwis {
 namespace Gdal {
-class GDALItems : public QSet<Resource>
+class GDALItems : public std::set<Resource>
 {
 public:
-    GDALItems(const QUrl& path, const UPCatalogConnector &containerc, IlwisTypes extTypes);
+    GDALItems(const QUrl& path, const QFileInfo &localFile, IlwisTypes extTypes);
 
 private:
     void addItem(const QUrl &url, quint64 csyid, quint64 grfId, IlwisTypes tp =itRASTER, IlwisTypes extTypes=itUNKNOWN);
-    quint64 addCsy(GdalHandle* handle, const QFileInfo &path, const QUrl &url);
+    quint64 addCsy(GdalHandle* handle, const QFileInfo &path, const QUrl &url, bool message=true);
 };
 }
 }
