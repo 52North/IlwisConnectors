3e9ab255dc5efc131102f73ad64a2900bcbb9454
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-05-19 08:36:39 +0200

    add csy to fromWkt call

--- pythonapi/pythonapi_geometry.cpp
+++ pythonapi/pythonapi_geometry.cpp
@@ -35,7 +35,7 @@ Geometry::Geometry(std::string wkt, const CoordinateSystem& csy):
     _standalone(true),
     _feature(nullptr),
     _index(-1),
-    _ilwisGeometry(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt))){
+    _ilwisGeometry(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt), csy.ptr()->as<Ilwis::CoordinateSystem>())){
     Ilwis::GeometryHelper::setCoordinateSystem(this->_ilwisGeometry.get(),csy.ptr()->as<Ilwis::CoordinateSystem>().ptr());
 }
 
@@ -85,18 +85,18 @@ void Geometry::fromWKT(const std::string& wkt){
             this->_standalone = true;
             this->_feature = nullptr;
             this->_index = -1;
-            this->_ilwisGeometry.reset(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt)));
+            this->_ilwisGeometry.reset(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt), Ilwis::ICoordinateSystem()));
         }else{
             if (this->_feature != nullptr && (bool)this->_feature && this->_feature->__bool__()){
-                this->_feature->ptr()->set(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt)), this->_index);
+                this->_feature->ptr()->set(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt), Ilwis::ICoordinateSystem()), this->_index);
             }else
                 throw InvalidObject("invalid referenc to hosting feature of non-standalone geometry!");
         }
     }else{
         if (_standalone){
-            this->_ilwisGeometry.reset(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt)));
+            this->_ilwisGeometry.reset(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt), Ilwis::ICoordinateSystem()));
         }else{
-            this->_feature->ptr()->set(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt)), this->_index);
+            this->_feature->ptr()->set(Ilwis::GeometryHelper::fromWKT(QString::fromStdString(wkt), Ilwis::ICoordinateSystem()), this->_index);
         }
     }
 }
