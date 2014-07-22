b9d1d77af46ba4b0fe316ebe0cee0714b8c980d2
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-07-22 09:15:03 +0200

    added new command handlers

--- gatherer/gathererrequesthandler.h
+++ gatherer/gathererrequesthandler.h
@@ -6,6 +6,7 @@
 #include "httpserver/httpserver/httprequesthandler.h"
 
 class QSqlQuery;
+class QSqlDatabase;
 
 namespace Ilwis {
 namespace Gatherer {
@@ -20,10 +21,18 @@ public:
     void service(HttpRequest& request, HttpResponse& response);
     static HttpRequestHandler *create();
 
+    QString buildQuery(const QString fields, const QString& field, const HttpRequest &request);
 private:
     QString packResults(QSqlQuery& db) const;
-    void doSelection(const HttpRequest &request, const QString &selectField, HttpResponse &response);
+    void doSelection(const HttpRequest &request, const QString &field, HttpResponse &response);
     void selectTemplate(const QString &templateName, HttpResponse &response);
+    void doCreateRequestMap(HttpRequest &request, QString classificationField);
+    bool doQuery(const QString &query, QSqlDatabase &db, QSqlQuery &results);
+    void createRequestMap(QSqlQuery &results);
+    void doTemplateList(HttpResponse &response);
+    void modifyDatabase(const HttpRequest &request, HttpResponse &response);
+    bool openDatabase(QSqlDatabase &db);
+    void writeMessage(const QString &key, const QString &message, HttpResponse &response);
 };
 }
 }
