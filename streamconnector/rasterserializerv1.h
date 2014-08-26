8f3aca15c15d65adcccc57f5cd25667f07130ce7
Martin Schouwenburg <mschouwen@gmail.com>
2014-08-26 20:04:52 +0200

    renames from stream*datainterface.*

---
+++ streamconnector/rasterserializerv1.h
@@ -0,0 +1,25 @@
+#ifndef STREAMRASTERDATAINTERFACE_H
+#define STREAMRASTERDATAINTERFACE_H
+
+namespace Ilwis {
+namespace Stream {
+
+
+
+class RasterSerializerV1 : public CoverageSerializerV1
+{
+public:
+    RasterSerializerV1(QDataStream& stream);
+
+    bool store(IlwisObject *obj, int options);
+    bool loadMetaData(IlwisObject*obj, const IOOptions & options);
+    bool loadData(IlwisObject *data, const IOOptions &options);
+    static VersionedSerializer *create(QDataStream &stream);
+
+private:
+
+};
+}
+}
+
+#endif // STREAMRASTERDATAINTERFACE_H
