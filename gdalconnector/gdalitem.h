56d5fef5e53193c3ceee117eaf05d09a266e9268
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2014-10-23 15:47:58 +0200

    included calculation of file sizes(bytes) of objects

--- gdalconnector/gdalitem.h
+++ gdalconnector/gdalitem.h
@@ -9,12 +9,13 @@ public:
     GDALItems(const QUrl& path, const QFileInfo &localFile, IlwisTypes extTypes);
 
 private:
-    quint64 addItem(GdalHandle* handle, const QUrl &url, quint64 csyid, quint64 grfId, IlwisTypes tp =itRASTER, IlwisTypes extTypes=itUNKNOWN);
+    quint64 addItem(GdalHandle* handle, const QUrl &url, quint64 csyid, quint64 grfId, IlwisTypes tp =itRASTER, IlwisTypes extTypes=itUNKNOWN, quint64 sz=i64UNDEF);
     quint64 addCsy(GdalHandle* handle, const QString &path, const QUrl &url, bool message=true);
     QString dimensions(GdalHandle* handle) const;
     std::map<QString, QString> kvp2Map(char **kvplist);
     void handleComplexDataSet(const std::map<QString, QString> &datasetdesc);
     quint64 numbertype2domainid(const QString &numbertype) const;
+    quint64 findSize(const QFileInfo &inf);
 };
 }
 }
