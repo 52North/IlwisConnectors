5b9130c60f53a567bfa09269ddff40234265cae6
Henning Bredel <h.bredel@52north.org>
2014-11-28 14:24:12 +0100

    let gdal connector only handle local resources

--- gdalconnector/gdalobjectfactory.cpp
+++ gdalconnector/gdalobjectfactory.cpp
@@ -48,6 +48,10 @@ bool GdalObjectFactory::canUse(const Resource &resource) const
     if ( resource.url().scheme() == "ilwis") // can't use anything marked as internal
         return false;
 
+    QFileInfo localResource(resource.url().toLocalFile());
+    if ( !(localResource.isFile() || localResource.isDir()))
+        return false; // do not handle remote sources
+
     if (!gdal()->supports(resource))
         return false;
 
