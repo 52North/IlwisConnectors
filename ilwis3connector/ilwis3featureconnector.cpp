023971f58c0c3cd9365eafac16e7f2803807d385
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-01-27 15:50:35 +0100

    overwritten the values of coverage_key which was already correct part of the code removed

--- ilwis3connector/ilwis3featureconnector.cpp
+++ ilwis3connector/ilwis3featureconnector.cpp
@@ -279,7 +279,7 @@ void  FeatureConnector::addFeatures(map<quint32,vector<geos::geom::Geometry *>>&
 
         ++rec;
     }
-    if ( featureValues.size() == 0){
+    if ( featureValues.size() == 0 && coldef.datadef().domain()->valueType() == itINDEXEDITEM){
         coldef.datadef().range<IndexedIdentifierRange>()->add(new IndexedIdentifier("",0,rec));
     }
 }
@@ -417,14 +417,14 @@ bool FeatureConnector::loadData(Ilwis::IlwisObject *obj, const IOOptions &) {
 
          if ( ok && extTable.isValid()) {
              ITable attTbl = fcoverage->attributeTable();
-             quint32 keyIndex = extTable->columnIndex(COVERAGEKEYCOLUMN);
+             //quint32 keyIndex = extTable->columnIndex(COVERAGEKEYCOLUMN);
              for(quint32 rowExt=0; rowExt < extTable->recordCount(); ++rowExt) {
                  if ( rowExt < fcoverage->featureCount()){
                      vector<QVariant> rec = extTable->record(rowExt);
-                     if ( keyIndex == iUNDEF) {// a pristine external table doesnt have a coverage key column which is present in the attributes of the feature coverage
-                         rec.resize(rec.size() + 1);
-                         rec[rec.size() - 1] = rowExt;
-                     }
+//                     if ( keyIndex == iUNDEF) {// a pristine external table doesnt have a coverage key column which is present in the attributes of the feature coverage
+//                         rec.resize(rec.size() + 1);
+//                         rec[rec.size() - 1] = rowExt;
+//                     }
                      attTbl->record(rowExt,rec);
                  }
              }
