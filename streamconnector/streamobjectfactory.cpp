862a2982df8d4fe7d96fd84eee25ed933570ba0f
Martin Schouwenburg <mschouwen@gmail.com>
2015-03-22 22:31:32 +0100

    now also loads streamed files from disk

--- streamconnector/streamobjectfactory.cpp
+++ streamconnector/streamobjectfactory.cpp
@@ -29,8 +29,17 @@ StreamObjectFactory::StreamObjectFactory() : IlwisObjectFactory("IlwisObjectFact
 bool StreamObjectFactory::canUse(const Resource &resource) const
 {
     QUrlQuery query(resource.url());
-    if ( query.queryItemValue("service") != "ilwisobjects") // can't use anything marked as internal
-        return false;
+    if ( query.queryItemValue("service") != "ilwisobjects") {
+        if ( resource.url().scheme() == "file") { // we might have a streamed object
+            QFileInfo inf(resource.url().toLocalFile());
+            const Module *module = kernel()->module("StreamModule");
+            if ( inf.suffix() != module->getInterfaceVersion()){
+                return false;
+            }
+
+        }else
+            return false;
+    }
 
     IlwisTypes type = resource.ilwisType() ;
     if ( type & itDOMAIN)
