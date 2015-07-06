20c99d2d836198f905909027da3487c140c9f967
MartinSchouwenburg <m.schouwenburg@utwente.nl>
2015-07-06 10:01:13 +0200

    some typos and the last parameter had index 3 while 4 was needed

--- opencvconnector/gaussianblurfilter.cpp
+++ opencvconnector/gaussianblurfilter.cpp
@@ -65,7 +65,7 @@ Ilwis::OperationImplementation::State GaussianBlurFilter::prepare(ExecutionConte
 
 quint64 GaussianBlurFilter::createMetadata()
 {
-    OperationResource operation({"ilwis://operations/gaussianglurfilter"},"opencv");
+    OperationResource operation({"ilwis://operations/gaussianblurfilter"},"opencv");
     operation.setSyntax("gaussianblurfilter(inputraster, kernelsizeX, kernelsizeY, sigmaX[,sigmaY]");
     operation.setDescription(TR(" blurring an image by a Gaussian function to reduce image noise and reduce detail"));
     operation.setInParameterCount({4,5});
@@ -73,7 +73,7 @@ quint64 GaussianBlurFilter::createMetadata()
     operation.addInParameter(1,itINTEGER , TR("kernel width"),TR("This is the neighborhood where the local variance will be calculated, and where pixels will contribute (in a weighted manner)"));
     operation.addInParameter(2,itINTEGER , TR("kernel height"),TR("This is the neighborhood where the local variance will be calculated, and where pixels will contribute (in a weighted manner)"));
     operation.addInParameter(3,itDOUBLE , TR("sigmax"),TR("gaussian kernel standard deviation in X direction.If both sigmas are zeros, they are computed from the kernel size"));
-    operation.addInParameter(3,itDOUBLE , TR("sigmay"),TR("gaussian kernel standard deviation in Y direction. If set to 0, the SigmaX value is taken optional"));
+    operation.addInParameter(4,itDOUBLE , TR("sigmay"),TR("gaussian kernel standard deviation in Y direction. If set to 0, the SigmaX value is taken optional"));
     operation.setOutParameterCount({1});
     operation.addOutParameter(0,itRASTER, TR("output raster"),TR("a raster with blurred features and reduced noise"));
     operation.setKeywords("image processing,raster,noise reduction, filter");
