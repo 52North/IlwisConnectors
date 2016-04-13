5aef963af0ad3b00ddd5db7e272225324df6bfdd
unknown <retsios@UTWKS61991.ad.utwente.nl>
2016-04-13 18:13:49 +0200

    Changed to quint64 just as the ilwistypes.h counterparts; enum is only good for 4 bytes, not enough to store a quint64.

--- pythonapi/pythonapi_extension.h
+++ pythonapi/pythonapi_extension.h
@@ -14,25 +14,24 @@ namespace pythonapi{
 
     //ilwisTypes
     struct it{
-        enum ilwisType{
-            FEATURE = itFEATURE,
-            RASTER = itRASTER,
-            COVERAGE = itCOVERAGE,
-            TABLE = itTABLE,
-            GEOREF = itGEOREF,
-            COORDSYSTEM = itCOORDSYSTEM,
-            DOMAIN = itDOMAIN,
-            NUMERICDOMAIN = itNUMERICDOMAIN,
-            ITEMDOMAIN = itITEMDOMAIN,
-            SINGLEOPERATION = itSINGLEOPERATION,
-            WORKFLOW = itWORKFLOW,
-            PROJECTION = itPROJECTION,
-            ELLIPSOID = itELLIPSOID,
-            CATALOG = itCATALOG,
-            CONTINUOUSCOLOR = itCONTINUOUSCOLOR,
-            PALETTECOLOR = itPALETTECOLOR,
-            VALUERANGE = itVALUERANGE
-        };
+        static const quint64 FEATURE = itFEATURE;
+        static const quint64 RASTER = itRASTER;
+        static const quint64 COVERAGE = itCOVERAGE;
+        static const quint64 TABLE = itTABLE;
+        static const quint64 GEOREF = itGEOREF;
+        static const quint64 COORDSYSTEM = itCOORDSYSTEM;
+        static const quint64 DOMAIN = itDOMAIN;
+        static const quint64 NUMERICDOMAIN = itNUMERICDOMAIN;
+        static const quint64 ITEMDOMAIN = itITEMDOMAIN;
+        static const quint64 SINGLEOPERATION = itSINGLEOPERATION;
+        static const quint64 WORKFLOW = itWORKFLOW;
+        static const quint64 PROJECTION = itPROJECTION;
+        static const quint64 ELLIPSOID = itELLIPSOID;
+        static const quint64 CATALOG = itCATALOG;
+        static const quint64 CONTINUOUSCOLOR = itCONTINUOUSCOLOR;
+        static const quint64 PALETTECOLOR = itPALETTECOLOR;
+        static const quint64 VALUERANGE = itVALUERANGE;
+        static const quint64 COLLECTION = itCOLLECTION;
     };
 }
 #endif // PYTHONAPI_EXTENSION_H
