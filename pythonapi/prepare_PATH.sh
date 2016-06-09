84d37d7f73d5ecf3cafed9f9253c08e2a7f73696
unknown <retsios@UTWKS61991.ad.utwente.nl>
2016-06-09 15:13:32 +0200

    cleanup of unused files

--- pythonapi/prepare_PATH.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Name: prepare_PATH
-
-#PATH=<DirOfPythonExecutable>:<DirOfQtBinaries>:<DirOfIlwisBinaries>
-PATH="/c/Python33:/c/Qt/Qt5.1.1/5.1.1/mingw48_32/bin:/c/Users/Poku/dev/Ilwis4/projects/output/win32debug/bin"
-
