eee8c19e04431d498d777082867fe19613bb9966
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2016-10-17 08:12:39 +0200

    the test for the prepare from a filename should not be preceeded by a a code construct. The url is always and url, never a code only

--- ilwis3connector/coverageconnector.cpp
+++ ilwis3connector/coverageconnector.cpp
@@ -405,7 +405,7 @@ DataDefinition CoverageConnector::determineDataDefintion(const ODF& odf,  const 
         }
     } else
         filename = odf->url(); // probe if it is an internal domain
-    if (!dom.prepare("code=domain:" + filename, options)) {
+    if (!dom.prepare(filename, options)) {
         ERROR2(ERR_NO_INITIALIZED_2,"domain",odf->url());
         return DataDefinition();
     }
