ad86351ff7ae89ac1bd7db33a5f2eb7c53b9d464
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2017-03-28 14:34:35 +0200

    new

--- streamconnector/combinationmatrixserializerv1.h
+++ streamconnector/combinationmatrixserializerv1.h
@@ -2,10 +2,18 @@
 #define COMBINATIONMATRIXSERIALIZERV1_H
 
 
-class CombinationMatrixSerializerv1
+namespace Ilwis {
+namespace Stream {
+class CombinationMatrixSerializerv1 : public VersionedSerializer
 {
 public:
-    CombinationMatrixSerializerv1();
-};
+    CombinationMatrixSerializerv1(QDataStream &stream) ;
 
+    bool store(IlwisObject *obj, const Ilwis::IOOptions &options= IOOptions());
+    bool loadMetaData(IlwisObject*obj, const IOOptions & options);
+    bool loadData(IlwisObject *data, const IOOptions &options);
+    static VersionedSerializer *create(QDataStream &stream);
+};
+}
+}
 #endif // COMBINATIONMATRIXSERIALIZERV1_H
